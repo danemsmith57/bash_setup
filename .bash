54ff5bc1565a9ca5b8181be4ced0bc5f12292816
danemsmith57 <55668408+danemsmith57@users.noreply.github.com>
2025-11-16 10:24:02 -0600

    Update .bash

--- .bash
+++ .bash
@@ -1,30 +1,33 @@
-green="\033[0;32m"
-blue="\033[0;34m"
-purple="\033[0;35m"
-red="\033[0;31m"
-no_color="\033[0m"
+green="\[\033[01;32m\]"
+blue="\[\033[01;34m\]"
+purple="\[\033[01;35m\]"
+red="\[\033[01;31m\]"
+no_color="\[\033[00m\]"
 
 parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^[*]/d' -e 's/* \(.*\)/(\1)/'
+  git branch 2>/dev/null | sed -n 's/^\* \(.*\)/(\1)/p'
 }
 
-custom_prompt(){
+custom_prompt() {
   PS1=""
+
   if [[ $CONDA_DEFAULT_ENV ]]; then
     PS1+="${purple}($CONDA_DEFAULT_ENV)${no_color}"
   fi
 
   git_info=$(parse_git_branch)
-  
-  if [[ $git_info ]]; then
-    PS1+="${red}${git_info:0:10}${no_color}
-    if [[ ${#git_info} -gt "10" ]]; then
-      PS1+="${red}...)${no_color}"
-    fi
-    PS1+=" "
-  fi
-  PS1="${green}\u@\h${no_color}"
-  PS1="${blue}\u@\h${no_color}"
-  PS1="$ "
+
+        if [[ $git_info ]]; then
+          branch="${git_info:1:-1}"  # remove ( )
+          trunc="${branch:0:10}"
+
+          if [[ ${#branch} -gt 10 ]]; then
+            PS1+=" ${red}(${trunc}...)${no_color}"
+          else
+            PS1+=" ${red}(${branch})${no_color}"
+          fi
+        fi
+
+  PS1+=" ${green}\u@\h${no_color}"
+  PS1+="$ "
 }
-PROMPT_COMMAND=custom_prompt
